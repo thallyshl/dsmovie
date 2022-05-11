bf96ffb52788f70dc9967d22c9a8cb54e4886ccb
Thallys <thallys10@gmail.com>
2022-05-11 03:06:37 +0100

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,11 +3,29 @@ import MovieStars from "components/MovieStars";
 import MovieScore from "components/MovieScore";
 import MovieCard from "components/MovieCard";
 
-function Listing () {
+function Listing() {
     return (
         <>
-        <Pagination />
-        <MovieCard />
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
