3214c1c2e62282f13481953383d73d84a742761c
Thallys <thallys10@gmail.com>
2022-05-11 02:53:13 +0100

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,14 @@
 import Pagination from "components/Pagination";
+import MovieStars from "components/MovieStars";
+import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 
 function Listing () {
     return (
+        <>
         <Pagination />
+        <MovieCard />
+        </>
     );
 }
 
