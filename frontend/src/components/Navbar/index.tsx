130c4d492e8b3a0b343f657fb892c2d928ae1124
Thallys <thallys10@gmail.com>
2022-05-15 21:28:03 +0100

    Final

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,11 +7,11 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <h1>THMovie</h1>
                     <a href="https://github.com/thallyshl">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className=" dsmovie-contact-link">/devsuperior</p>
+                            <p className=" dsmovie-contact-link">/thallyshl</p>
                         </div>
                     </a>
                 </div>
