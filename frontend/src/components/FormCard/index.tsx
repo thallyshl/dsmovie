0677f0f801aaf6a4492b2506c388a9d8f5ee7f2d
Thallys <thallys10@gmail.com>
2022-05-15 22:17:33 +0100

    fix validate1

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -16,7 +16,7 @@ function FormCard( {movieId} : Props) {
     const [movie, setMovie] = useState<Movie>();
 
     useEffect(() => {
-        axios.get(`{BASE_URL}/movies/${movieId}`)
+        axios.get(`${BASE_URL}/movies/${movieId}`)
             .then(response => {
                 setMovie(response.data);
             })
